17fbf4dcc41ed0904c07086e1ca7e87dc6849af9
eben <ebencito88@gmail.com>
2023-12-06 18:45:36 +0000

    Pet Service Done

---
+++ projet-3/src/services/pet.services.js
@@ -0,0 +1,85 @@
+import api from "./config"
+
+const getAllPets = async () => {
+  try {
+    const { data } = await api.get("/pets")
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getOnePet = async (id) => {
+  try {
+    const { data } = await api.get(`/pets/${id}`)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getOwnPets = async () => {
+  try {
+    const { data } = await api.get("/pets/me")
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const createOwnPet = async (body) => {
+  try {
+    const { data } = await api.post("/pets/me",body)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const createPet = async (body) => {
+  try {
+    const { data } = await api.post("/pets",body)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const updateOwnPet = async (id,body) => {
+  try {
+    const { data } = await api.put(`/pets/me/${id}`,body)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const updatePet = async (id,body) => {
+  try {
+    const { data } = await api.post(`/pets/${id}`,body)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const deleteOwnPet = async (id,body) => {
+  try {
+    const { data } = await api.delete(`/pets/me/${id}`,body)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const deletePet = async (id,body) => {
+  try {
+    const { data } = await api.delete(`/pets/${id}`,body)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export { getAllPets, getOnePet, getOwnPets, createOwnPet, createPet, updateOwnPet, updatePet, deleteOwnPet, deletePet  }
+
