05beb774a746989ea7e55013d23e13d7b56f6ecc
eben <ebencito88@gmail.com>
2023-12-07 18:14:28 +0000

    merge commit

--- projet-3/src/pages/Home/Home.jsx
+++ projet-3/src/pages/Home/Home.jsx
@@ -1,27 +1,10 @@
 import './Home.css'
-import { useState, useEffect } from "react"
+
 import React from 'react'
-import { getAllPets } from '../../services/pet.services'
 
 function Home() {
-  const [pets, setPets] = useState([])
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await getAllPets()
-      setPets(result)
-    }
-    fetchData()
-  }, [])
   return (
-    <div>
-       <h1>Lista de mascotas</h1>
-      <ul>
-        {pets.map((pet) => (
-          <li key={pet.id}>{pet.name}</li>
-        ))}
-      </ul>
-
-    </div>
+    <></>
   )
 }
 
