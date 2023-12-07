a96a4000acd27a2d31f64f5867d2d1f6d4076783
eben <ebencito88@gmail.com>
2023-12-07 16:57:45 +0000

    User Service Added

--- projet-3/src/services/pet.services.js
+++ projet-3/src/services/pet.services.js
@@ -56,7 +56,7 @@ const updateOwnPet = async (id,body) => {
 
 const updatePet = async (id,body) => {
   try {
-    const { data } = await api.post(`/pets/${id}`,body)
+    const { data } = await api.put(`/pets/${id}`,body)
     return data
   } catch (error) {
     console.log(error)
