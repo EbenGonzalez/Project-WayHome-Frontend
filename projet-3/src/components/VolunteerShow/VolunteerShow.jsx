848a7b7141474106034058c0deacfdcd19ae60e6
eben <ebencito88@gmail.com>
2023-12-08 12:01:10 +0000

    Rating Test Added

--- projet-3/src/components/VolunteerShow/VolunteerShow.jsx
+++ projet-3/src/components/VolunteerShow/VolunteerShow.jsx
@@ -1,12 +1,14 @@
 import "./VolunteerShow.css"
 
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from 'react'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import BasicRating from "../Rating/Rating"
+
 
 export default function VolunteerShow({volunteer}) {
   return (
@@ -24,11 +26,12 @@ export default function VolunteerShow({volunteer}) {
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
         </Typography>
+        <BasicRating/>
       </CardContent>
       <CardActions>
         <Button size="small">Share</Button>
         <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
-  );
+  )
 }
