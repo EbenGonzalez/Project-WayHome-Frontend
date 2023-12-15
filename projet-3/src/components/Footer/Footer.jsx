9d902f0ab0cdd3cddce4dd8236833af16cb9aa0b
RobertoNeiraGonzalez <roberneira31@gmail.com>
2023-12-15 19:53:56 +0000

    newChanges Footer

--- projet-3/src/components/Footer/Footer.jsx
+++ projet-3/src/components/Footer/Footer.jsx
@@ -1,44 +1,27 @@
 import './Footer.css'
-// import FacebookIcon from '@mui/icons-material/Facebook';
-// import InstagramIcon from '@mui/icons-material/Instagram';
-// import YouTubeIcon from '@mui/icons-material/YouTube';
-// import LinkedInIcon from '@mui/icons-material/LinkedIn';
-// import GitHubIcon from '@mui/icons-material/GitHub';
-// import PetsIcon from '@mui/icons-material/Pets';
-import { Link } from 'react-router-dom'
-import {
-  Facebook as FacebookIcon,
-  Instagram as InstagramIcon,
-  YouTube as YouTubeIcon,
-  LinkedIn as LinkedInIcon,
-  GitHub as GitHubIcon,
-  Pets as PetsIcon
-} from '@mui/icons-material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import PetsIcon from '@mui/icons-material/Pets';
 
-import {
-  Box,
-  Grid,
-  Container,
-  Typography,
-  Button
-} from '@mui/material'
+import React from 'react'
 
+import { Box, Grid, Container, Typography, Button } from '@mui/material'
 
 function Footer() {
   const elements = [
     {
       header: 'Contacto',
-      path: '/contacto',
       links: [<div><FacebookIcon/><InstagramIcon/><YouTubeIcon/></div>]
     },
     {
       header: 'Login',
-      path: '/login',
       links: ['Registrarse']
     },
     {
       header: 'Quienes somos',
-      path: '/quienesSomos',
       links: [<div><LinkedInIcon/><GitHubIcon/><PetsIcon/></div>]
     }
   ]
@@ -46,22 +29,20 @@ function Footer() {
   function generateFooterElements() {
     const footerElements = elements.map((column, columnIndex) => {
       return (
-        <Link  key={columnIndex} to={column.path} xs={12} md={4}>
-          <Grid>
-              <Box borderBottom={1}>
-                <Button sx={{ color: 'white', fontWeight: 'bold' }}>
-                  {column.header}
-                </Button>
+        <Grid key={columnIndex} item xs={12} md={4}>
+          <Box borderBottom={1}>
+            <Button sx={{ color: 'white', fontWeight: 'bold' }}>
+              {column.header}
+            </Button>
+          </Box>
+          {column.links.map((link, linkIndex) => {
+            return (
+              <Box key={linkIndex}>
+                <Button sx={{ color: 'white' }}>{link}</Button>
               </Box>
-            {column.links.map((link, linkIndex) => {
-              return (
-                <Box key={linkIndex}>
-                  <Button sx={{ color: 'white' }}>{link}</Button>
-                </Box>
-              )
-            })}
-          </Grid>
-        </Link>
+            )
+          })}
+        </Grid>
       )
     })
     return footerElements
@@ -69,34 +50,34 @@ function Footer() {
 
   return (
     <footer>
-      <Box
-        textAlign={'center'}
-        component="footer"
-        bgcolor="primary.main"
-        color="white"
-        py={2}
-        sx={{
-
-          bottom: 0,
-          width: '100%',
-          height: '20%'
-        }}>
+      <Box  
+      textAlign={'center'}
+      component="footer"
+      bgcolor="primary.main"
+      color="white"
+      py={2}
+      sx={{
+       
+        bottom: 0,
+        width: '100%',
+        height: '20%'
+      }}>
         <Container>
-          <Grid container columnSpacing={0} justifyContent="center" >
+          <Grid container columnSpacing={40}>
             {generateFooterElements()}
           </Grid>
         </Container>
       </Box>
-      <Box textAlign={'center'}
-        py={2} m={0}
-        bgcolor='#118da0'
-        color={'white'}
-        sx={{
-
-          bottom: 0,
-          width: '100%',
-          height: '8%'
-        }}>
+      <Box textAlign={'center'} 
+      py={2} m={0} 
+      bgcolor='#118da0' 
+      color={'white'} 
+      sx={{
+        
+        bottom: 0,
+        width: '100%',
+        height: '8%'
+      }}>
         <Typography>© Way Home 2023 - Todos los derechos reservados</Typography>
       </Box>
     </footer>
