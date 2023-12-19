e8e9c357be3c893483b9884b1c236a48a6425272
Roberto Neira González <121897103+RobertoNeiraGonzalez@users.noreply.github.com>
2023-12-19 20:55:41 +0000

    changed mapa

--- projet-3/src/components/Mapa/Mapa.jsx
+++ projet-3/src/components/Mapa/Mapa.jsx
@@ -7,6 +7,16 @@ import DialogContent from '@mui/material/DialogContent'
 import DialogTitle from '@mui/material/DialogTitle'
 import LocationOnIcon from '@mui/icons-material/LocationOn'
 
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
+  iconUrl: require('leaflet/dist/images/marker-icon.png').default,
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
+});
+
 function Mapa({ volunteers }) {
   const [markers, setMarkers] = useState([])
   const [open, setOpen] = useState(false)
