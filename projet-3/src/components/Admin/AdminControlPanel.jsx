df141e21c4403baab8feea2a77c0687c6ece228c
eben <ebencito88@gmail.com>
2023-12-19 10:30:24 +0000

    Panel de Control

--- projet-3/src/components/Admin/AdminControlPanel.jsx
+++ projet-3/src/components/Admin/AdminControlPanel.jsx
@@ -1,35 +1,64 @@
-import React from 'react'
-import { Button, Container, Grid } from '@mui/material'
+import React, { useState } from 'react'
+import { Button, Container, Dialog, DialogActions, DialogContent, DialogTitle, Grid } from '@mui/material'
 import AdminAddUser from './AdminAddUser'
 
 const AdminPanel = () => {
-  const handleButtonClick = () => {
+  const [openModal, setOpenModal] = useState(false)
+
+  const handleModalOpen = () => {
+    setOpenModal(true)
+  }
+
+  const handleModalClose = () => {
+    setOpenModal(false)
+  }
+
+  const handleButtonClick = (buttonName) => {
+    handleModalClose()
   }
 
   return (
     <Container style={{ marginTop: '20px' }} maxWidth="md">
       <Grid container spacing={2} justifyContent="center">
         <Grid item>
-          <AdminAddUser/>
-        </Grid>
-        <Grid item>
-          <Button variant="contained" color="primary" onClick={() => handleButtonClick('Botón 2')}>
-            Botón 2
+          <Button variant="contained" color="primary" onClick={handleModalOpen}>
+            Panel de Control
           </Button>
-        </Grid>
-        <Grid item>
-          <Button variant="contained" color="primary" onClick={() => handleButtonClick('Botón 3')}>
-            Botón 3
-          </Button>
-        </Grid>
-        <Grid item>
-          <Button variant="contained" color="primary" onClick={() => handleButtonClick('Botón 4')}>
-            Botón 4
-          </Button>
+          <Dialog open={openModal} onClose={handleModalClose} aria-labelledby="dialog-title">
+            <DialogTitle id="dialog-title">Selecciona una opción</DialogTitle>
+            <DialogContent>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <AdminAddUser />
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" color="primary" onClick={() => handleButtonClick('Botón 2')}>
+                    Botón 2
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" color="primary" onClick={() => handleButtonClick('Botón 3')}>
+                    Botón 3
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" color="primary" onClick={() => handleButtonClick('Botón 4')}>
+                    Botón 4
+                  </Button>
+                </Grid>
+              </Grid>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleModalClose} color="primary">
+                Cerrar
+              </Button>
+            </DialogActions>
+          </Dialog>
         </Grid>
       </Grid>
     </Container>
   )
 }
 
-export default AdminPanel;
+export default AdminPanel
+
